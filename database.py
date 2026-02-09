077e9c0fe7a3b0b28ecb7c9b07aa65d93fbb5ea7
TRedBr <colin-brandes@gmx.de>
2026-02-09 12:41:58 +0100

    database.py-added:initialise_database()-function

--- database.py
+++ database.py
@@ -1,1 +1,24 @@
 import sqlite3
+
+def initialise_database():
+    conn = sqlite3.connect("datadir/datadirectory.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER,
+        name TEXT
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS quizzes (
+    id INTEGER,
+    Quizname TEXT,
+    )""")
+    cursor.execute("SELECT * FROM users")
+    for user in cursor.fetchall():
+        print(user)
+    conn.execute("SELECT * FROM quizzes")
+    for quiz in cursor.fetchall():
+        print(quiz)
+    conn.close()
+
